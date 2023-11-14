a626f12875c9d9755f4168e78061a4a9a5ebcb8c
Sebastian Kiolbassa <skiolbassa@Angelicas-MacBook-Pro.local>
2023-11-13 20:17:26 -0600

    Reverting back to commit 81d2564

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,64 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundry';
 import './App.css';
 
-function App() {
-    // constructor() {
-        // super()
-    //     this.state = {
-    //         robots: [],
-    //         searchfield: ""
-    //     }
-<<<<<<< HEAD
-    // } 
-=======
-    // }
->>>>>>> ec2c5b7abdca97bf7bd1befab0163e821131b6ca
-    const [robots, setRobots] = useState([])
-    const [searchfield, setSearchfield] = useState('')
-    
-    // componentDidMount() {
-    //     fetch('https://jsonplaceholder.typicode.com/users')
-    //         .then(response=> response.json())
-    //         .then(users => this.setState({ robots: users }));
-    //     }
-    useEffect(() => {
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ""
+        }
+    }
+
+    componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response=> response.json())
-            .then(users => {setRobots(users)});
-    })
+            .then(users => this.setState({ robots: users }));
+        }
+    
 
-<<<<<<< HEAD
-     onSearchChange = (event) => {
-        setSearchfield(event.target.value);
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
     }
 
-     filteredRobots = robots.filter(robot => {
-=======
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value);
-    }
-
-    const filteredRobots = robots.filter(robot => {
->>>>>>> ec2c5b7abdca97bf7bd1befab0163e821131b6ca
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-    return !robots.length ? 
-        <h1 className='tc'>Loading...</h1> :
-        (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots={filteredRobots} />
-                    </ErrorBoundary>
-                </Scroll>
-            </div>
-        );
-    }
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !robots.length ? 
+            <h1 className='tc'>Loading...</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundary>
+                    </Scroll>
+                </div>
+            );
+        }
+    }   
 
 export default App;
